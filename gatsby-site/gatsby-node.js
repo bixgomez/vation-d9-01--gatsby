afd8e82b837ccdb9c32d75cbaa780f7a597549f9
Richard Gilbert <gilbert@Melies.local>
2021-03-18 17:34:20 -0700

    Gatsby: Creating query to pull companies, template to display indiv. company.

--- gatsby-site/gatsby-node.js
+++ gatsby-site/gatsby-node.js
@@ -4,4 +4,31 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path')
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const companies = await graphql(`
+    {
+      allNodeCompany {
+        nodes {
+          id
+          path {
+            alias
+          }
+        }
+      }
+    }
+  `)
+
+  companies.data.allNodeCompany.nodes.map(companyData =>
+    createPage({
+      path: companyData.path.alias,
+      component: path.resolve('src/templates/company.js'),
+      context: {
+        CompanyID: companyData.id
+      },
+    })
+  )
+}
