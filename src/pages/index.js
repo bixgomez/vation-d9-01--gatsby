e026c99cd316427c4db4c5aeadb08f5c19edf66b
Richard Gilbert <gilbert@Melies.local>
2021-03-23 10:30:36 -0700

    Some minor changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,57 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react'
+import {graphql} from 'gatsby'
+import Layout from '../components/layout'
+import CompanyTeaser from "../components/companyTeaser"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const Companies = ({data}) => {
+    const companies = data.allNodeCompany.nodes
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+    return (
+        <Layout>
+            <h1>Companies</h1>
+            {companies.map(company => (
+                <CompanyTeaser
+                    key={company.id}
+                    title={company.title}
+                    path={company.path.alias}
+                    image={company.relationships.field_logo.localFile.childImageSharp.fluid}
+                    alt={company.field_logo.alt}
+                    summary={company.body.summary}
+                />
+            ))}
+        </Layout>
+    )
+}
 
-export default IndexPage
+export const query = graphql`
+  {
+    allNodeCompany(sort: {fields: title, order: ASC}) {
+      nodes {
+        id
+        title
+        body {
+          summary
+        }
+        field_logo {
+          alt
+        }
+        path {
+          alias
+        }
+        relationships {
+          field_logo {
+            localFile {
+              childImageSharp {
+                fluid(maxWidth: 500) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Companies
